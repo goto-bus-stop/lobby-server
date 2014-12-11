0befd327c8d29f364e3bded8535ad792abc26361
René Kooi <renekooi@outlook.com>
2014-12-11 17:41:54 +0100

    small winter cleanup

--- src/model/GameRoom.js
+++
@@ -1,8 +0,0 @@
-const store = require('../store')
-    , debug = require('debug')('aocmulti:GameRoom')
-
-module.exports = GameRoom
-
-function GameRoom() {
-  if (!(this instanceof GameRoom)) return new GameRoom()
-}
