cb11db65471f308dd90abc391753652b2290960a
René Kooi <rene@kooi.me>
2015-01-17 16:26:38 +0100

    start adding IRC server
    The irc server will be hooked up to express through a simple PubSub-like global event channel.
    Users logging in, joining rooms, leaving, and chatting will all go through IRC.
    Game start messages and the like will also pass through the IRC server but will be sent to actual users as PRIVMSGs from GameServ or similar.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import IRCServer from 'ircs'
 
 const debug = require('debug')('aocmulti:server')
 
 let app = express()
+let irc = IRCServer()
+
+app.set('irc', irc)
 
 import api from './routes'
 app.use('/_', api)
 
 app.listen(23649)
+irc.listen(6667)
