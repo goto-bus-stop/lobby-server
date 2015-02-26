a59b231b09f6db92b019c0774a86e4a5ad83230d
René Kooi <rene@kooi.me>
2015-02-26 12:12:18 +0100

    upgrade awestruct

--- src/routes/client-api.js
+++ src/routes/client-api.js
@@ -86,88 +86,95 @@ module.exports = function () {
     }
   })
 
+  const array = Struct.types.array
+      , char = Struct.types.char
+      , int8 = Struct.types.int8
+      , uint8 = Struct.types.uint8
+      , bool = Struct.types.uint8.transform(function (val) { return val !== 0 })
+      , uint16 = Struct.types.uint16
+      , uint32 = Struct.types.uint32
   const gameStatsStruct = Struct({
-    u1: Struct.array(44, 'uint8'),
-    scenarioName: Struct.char(32),
-    u2: Struct.array(4, 'uint8'),
+    u1: array(44, uint8),
+    scenarioName: char(32),
+    u2: array(4, uint8),
     duration: 'uint32',
-    cheatsEnabled: 'bool',
-    complete: 'uint8',
-    u3: Struct.array(14, 'uint8'),
-    mapSize: 'uint8',
-    map: 'uint8',
-    pop: 'uint8',
-    u4: 'uint8',
-    victory: 'uint8',
-    startAge: 'uint8',
-    resources: 'uint8',
-    allTech: 'bool',
-    teamTogether: 'bool',
-    revealMap: 'uint8',
-    u5: Struct.array(3, 'uint8'),
-    lockTeams: 'bool',
-    lockSpeed: 'bool',
+    cheatsEnabled: bool,
+    complete: uint8,
+    u3: array(14, uint8),
+    mapSize: uint8,
+    map: uint8,
+    pop: uint8,
+    u4: uint8,
+    victory: uint8,
+    startAge: uint8,
+    resources: uint8,
+    allTech: bool,
+    teamTogether: bool,
+    revealMap: uint8,
+    u5: array(3, uint8),
+    lockTeams: bool,
+    lockSpeed: bool,
     u6: 'int8',
-    playerStats: Struct.array(8, Struct({
-      name: Struct.char(16),
-      totalScore: 'uint16',
-      totalScores: Struct.array(8, 'uint16'),
-      u0: 'uint8',
-      civilization: 'uint8',
-      index: 'uint8',
-      team: 'uint8',
-      u1: Struct.array(2, 'uint8'),
-      mvp: 'bool',
-      u2: Struct.array(3, 'uint8'),
-      result: 'uint8',
-      u3: Struct.array(3, 'uint8'),
+    playerStats: array(8, Struct({
+      name: char(16),
+      totalScore: uint16,
+      totalScores: array(8, uint16),
+      u0: uint8,
+      civilization: uint8,
+      index: uint8,
+      team: uint8,
+      u1: array(2, uint8),
+      mvp: bool,
+      u2: array(3, uint8),
+      result: uint8,
+      u3: array(3, uint8),
       militaryStats: Struct({
-        militaryScore: 'uint16',
-        kills: 'uint16',
-        u1: 'uint16',
-        unitsLost: 'uint16',
-        razes: 'uint16',
-        u2: 'uint16',
-        buildingsLost: 'uint16',
-        conversions: 'uint16'
+        militaryScore: uint16,
+        kills: uint16,
+        u1: uint16,
+        unitsLost: uint16,
+        razes: uint16,
+        u2: uint16,
+        buildingsLost: uint16,
+        conversions: uint16
       }),
-      u4: Struct.array(32, 'uint8'),
+      u4: array(32, uint8),
       economyStats: Struct({
-        economyScore: 'uint16',
-        u1: 'uint16',
-        foodCollected: 'uint32',
-        woodCollected: 'uint32',
-        stoneCollected: 'uint32',
-        goldCollected: 'uint32',
-        tributeSent: 'uint16',
-        tributeRcvd: 'uint16',
-        tradeProfit: 'uint16',
-        relicGold: 'uint16'
+        economyScore: uint16,
+        u1: uint16,
+        foodCollected: uint32,
+        woodCollected: uint32,
+        stoneCollected: uint32,
+        goldCollected: uint32,
+        tributeSent: uint16,
+        tributeRcvd: uint16,
+        tradeProfit: uint16,
+        relicGold: uint16
       }),
-      u5: Struct.array(16, 'uint8'),
+      u5: array(16, uint8),
       technologyStats: Struct({
-        technologyScore: 'uint16',
-        u1: 'uint16',
-        feudalAge: 'uint32',
-        castleAge: 'uint32',
-        imperialAge: 'uint32',
-        mapExplored: 'uint8',
-        researchCount: 'uint8',
-        researchPercent: 'uint8'
+        technologyScore: uint16,
+        u1: uint16,
+        feudalAge: uint32,
+        castleAge: uint32,
+        imperialAge: uint32,
+        mapExplored: uint8,
+        researchCount: uint8,
+        researchPercent: uint8
       }),
-      u6: 'int8',
+      u6: int8,
       societyStats: Struct({
-        societyScore: 'uint16',
-        totalWonders: 'uint8',
-        totalCastles: 'uint8',
-        relicsCaptured: 'uint8',
-        u1: 'uint8',
-        villagerHigh: 'uint16'
+        societyScore: uint16,
+        totalWonders: uint8,
+        totalCastles: uint8,
+        relicsCaptured: uint8,
+        u1: uint8,
+        villagerHigh: uint16
       }),
-      u7: Struct.array(84, 'uint8')
+      u7: array(84, uint8)
     })),
-    playerIndex: 'uint8',
-    u7: Struct.array(7, 'uint8')
+    playerIndex: uint8,
+    u7: array(7, uint8)
   })
   app.post('/game/:seskey/end', function (req, res) {
     var data = new Buffer(2140)
